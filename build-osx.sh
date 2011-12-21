e3ae51bd5a2f99e10bf4598ecc13638129b9ea7d
Romain Ruetschi <romain.ruetschi@gmail.com>
2011-12-21 21:48:02 +0100

    Fix a typo and remove the packages installation part.

--- build-osx.sh
+++ build-osx.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 BUNDLE="$PWD/Lonely Planet.app"
-PACKAGES="cairo gdk-pixbuf glib libcroco librsvg pango pixman sdl"
+LIBS_PATH="/usr/local/lib"
 LIBS="libcroco-0.6.3.0.1.dylib
 libgdk_pixbuf-2.0.0.dylib
 libgio-2.0.0.dylib
@@ -15,20 +15,13 @@ libpangoft2-1.0.0.dylib
 libpixman-1.0.24.0.dylib
 librsvg-2.2.dylib"
 
-echo "Installing required homebrew packages: $PACKAGES"
-echo "Note: This will only have an effect for missing packages."
-for package in $PACKAGES; do
-    brew install $package
-done
-echo "Done."
-
 echo "Creating a copy of the bundle skeleton..."
 ditto "$PWD/osx/Skeleton.app" "$BUNDLE"
-echo "Done."
+echo -n "Done."
 
-echo "Copying required lib from /usr/local/lib into the bundle..."
+echo -n "Copying required lib from /usr/local/lib into the bundle..."
 for lib in $LIBS; do
-    ditto /usr/local/lib/$lib "$BUNDLE/Contents/Resources/lib/"
+    ditto $LIBS_PATH/$lib "$BUNDLE/Contents/Resources/lib/"
 done
 echo "Done."
 
@@ -38,9 +31,9 @@ rock -g -v "+-arch" "+x86_64" "+-Wl,-framework,Cocoa" -lSDLmain -lSDL
 echo "Done."
 
 echo "Copying configuration, assets and executable into the bundle..."
-ditto $PWD/alone "./$BUNDLE/Contents/Resources/"
-ditto $PWD/alone.config "./$BUNDLE/Contents/Resources/"
-ditto $PWD/assets "./$BUNDLE/Contents/Resources/assets"
-echo "Done."
+ditto $PWD/alone "$BUNDLE/Contents/Resources/"
+ditto $PWD/alone.config "$BUNDLE/Contents/Resources/"
+ditto $PWD/assets "$BUNDLE/Contents/Resources/assets"
+echo -n "Done."
 
 echo " [ SUCCESS ] !"
