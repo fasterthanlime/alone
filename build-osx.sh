6bc62a21a7e60243e42f27cbdcde7e8652f36aa3
Romain Ruetschi <romain.ruetschi@gmail.com>
2011-12-21 21:39:38 +0100

    build-osx.sh now builds a proper Mac OS X bundle (.app).

--- build-osx.sh
+++ build-osx.sh
@@ -1,1 +1,46 @@
-rock -v "+-arch" "+x86_64" "+-Wl,-framework,Cocoa" -lSDLmain -lSDL
+#!/usr/bin/env bash
+
+BUNDLE="$PWD/Lonely Planet.app"
+PACKAGES="cairo gdk-pixbuf glib libcroco librsvg pango pixman sdl"
+LIBS="libcroco-0.6.3.0.1.dylib
+libgdk_pixbuf-2.0.0.dylib
+libgio-2.0.0.dylib
+libglib-2.0.0.dylib
+libgmodule-2.0.0.dylib
+libgobject-2.0.0.dylib
+libgthread-2.0.0.dylib
+libpango-1.0.0.dylib
+libpangocairo-1.0.0.dylib
+libpangoft2-1.0.0.dylib
+libpixman-1.0.24.0.dylib
+librsvg-2.2.dylib"
+
+echo "Installing required homebrew packages: $PACKAGES"
+echo "Note: This will only have an effect for missing packages."
+for package in $PACKAGES; do
+    brew install $package
+done
+echo "Done."
+
+echo "Creating a copy of the bundle skeleton..."
+ditto "$PWD/osx/Skeleton.app" "$BUNDLE"
+echo "Done."
+
+echo "Copying required lib from /usr/local/lib into the bundle..."
+for lib in $LIBS; do
+    ditto /usr/local/lib/$lib "$BUNDLE/Contents/Resources/lib/"
+done
+echo "Done."
+
+echo "Compiling the game..."
+source $PWD/devrc
+rock -g -v "+-arch" "+x86_64" "+-Wl,-framework,Cocoa" -lSDLmain -lSDL
+echo "Done."
+
+echo "Copying configuration, assets and executable into the bundle..."
+ditto $PWD/alone "./$BUNDLE/Contents/Resources/"
+ditto $PWD/alone.config "./$BUNDLE/Contents/Resources/"
+ditto $PWD/assets "./$BUNDLE/Contents/Resources/assets"
+echo "Done."
+
+echo " [ SUCCESS ] !"
